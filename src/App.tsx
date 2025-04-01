c846a28fbfe92dd784bd18e00955dfe42eb0cdab
Sasha Ivanova <sashaivanova.pro@gmail.com>
2025-04-01 16:48:40 +0400

    fix: change deployment configuration for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/urban-market">
       <div className="app">
         <Header />
         <main className="app-main">
